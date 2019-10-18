fb7621452f624b930db647341cbe63ecf383a5e3
Guille19 <guille19@debian>
2019-10-18 17:36:56 -0600

    Labo 8 Terminado

--- Laboratorios/Laboratorio8/JS/main.js
+++ Laboratorios/Laboratorio8/JS/main.js
@@ -2,24 +2,85 @@ let carnet_field = document.querySelector("#carnet_field");
 let schedule_dropdown = document.querySelector("#schedule_field");
 let late_switch = document.querySelector("#late_switch");
 let submit_btn = document.querySelector("#submit_btn");
-let journalL = [];
+
 let table_body = document.querySelector("#table_body");
-let carnet_regex = new RegExp('[0-9]{8}');
+let carnet_regex = new RegExp("^[0-9]{8}$");
 
-let addStudent = (carnet, schedule, late) =>{
-    let new_row = document.createElement("tr");
-    let datetime = new Date();
+let student_list = [];
+let serial = 0;
 
-    new_row.className="table_active"
-    new_row.innerHTML = `<th scope='row'>${carnet}</th>
-    <td>${schedule}</td>
-    <td>${datetime}</td>
-    <td>${late}</td>
-    `;
+let printArray = ()=>{
+    table_body.innerHTML = "";
 
-    table_body.appendChild(new_row);
+    student_list.forEach((element)=>{
+        let new_row = document.createElement("tr");
+        new_row.className = "table_active;"
+        new_row.innerHTML = `<th scope='row'>${element.carnet}</th>
+            <td>${element.schedule}</td>
+            <td>${element.ingreso}</td>
+            <td>${element.late}</td>`;
 
-    
+        table_body.appendChild(new_row);
+
+        let new_cell = document.createElement("td")
+        let new_btn = document.createElement("button")
+        let new_TextInput = document.createElement("input")
+
+
+    /*
+        personalizacion del boton
+    */ 
+        
+        new_btn.className = "btn btn-danger"
+        new_btn.innerText = "Hola Mundo"
+        new_btn.value = element.id;
+
+        new_btn.addEventListener("click", event=>{
+            let id_actual = event.target.value
+
+            
+                student_list.forEach( (element, pos)=>{
+                    if(new_TextInput.value == element.carnet){
+                        if(id_actual == element.id){
+                            console.log(element.carnet);
+                            student_list.splice(pos, 1)         
+                    
+                           printArray()
+                        }
+                    }
+                })
+            
+        })
+
+    new_cell.appendChild(new_btn)
+    new_row.appendChild(new_cell)
+    table_body.appendChild(new_row)
+
+    new_TextInput.className = "text"
+
+    new_cell = document.createElement("td")
+    new_cell.appendChild(new_TextInput)
+    new_row.appendChild(new_cell)
+    table_body.appendChild(new_row)
+
+    })
+
+}
+
+/*Se dice esquedchul*/ 
+    let add_student = (carnet, schedule, late)=>{
+    let datetime = new Date;
+    let datetime_string = datetime.toLocaleString();
+
+    student_list.push({
+        "id": serial,
+        "carnet": carnet,
+        "horario":schedule,
+        "tarde":late,
+        "ingreso": datetime_string
+    });
+
+    serial ++;
 }
 
 let parseLateSwitch = (value)=>{
@@ -28,7 +89,8 @@ let parseLateSwitch = (value)=>{
     }
     return "A tiempo"
 }
-
+/*
+añadir una sexta columna para eliminar, pero con input de texto*/ 
 
 submit_btn.addEventListener("click", ()=>{
     let carnet = carnet_field.value;
@@ -36,13 +98,12 @@ submit_btn.addEventListener("click", ()=>{
     let late = parseLateSwitch(late_switch.checked);
 
     if(carnet_regex.test(carnet)){
-        addStudent(carnet,schedule, late);
+        add_student(carnet, schedule, late);
+        printArray()
     } else {
         alert("Formato del carnet no valido");
     }
 })
-
-
 
 carnet_field.addEventListener("keyup", (event)=>{
     let keyCode = event.keyCode;
@@ -56,5 +117,9 @@ carnet_field.addEventListener("keyup", (event)=>{
         submit_btn.disabled = false;
     } else {
         submit_btn.disabled = true;
-    }
+    }                                   
 })
+
+window.onload=()=>{
+
+}
